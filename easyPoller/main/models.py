3fe7f894e30a80a2490c797a229995e9e13f7ce3
Joseph Marotta <marottaj@bu.edu>
2025-01-11 22:39:16 -0500

    added models for polls and options

--- easyPoller/main/models.py
+++ easyPoller/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Poll(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class AnswerOptions(models.Model):
+    poll = models.ForeignKey(Poll, related_name="answer_option", on_delete=models.CASCADE)
+    option = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
