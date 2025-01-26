f00acf01901b31433ed01a24ec290624e2f8b5d0
Joseph Marotta <marottaj@bu.edu>
2025-01-26 18:42:52 -0500

    Added trailing slashes to the URLs

--- easyPoller/main/urls.py
+++ easyPoller/main/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path("", views.index),
     path("<slug:slug>", views.poll_view, name='poll_view'),
+    path("<slug:slug>/", views.poll_view, name='poll_view'),
     path("<slug:slug>/results", views.results_view),
+    path("<slug:slug>/results/", views.results_view),
     path('vote/', views.vote)
 ]
