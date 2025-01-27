4b9526c3b3a7d4d6f62c8ec907bf9ef66965ff30
Joseph Marotta <marottaj@bu.edu>
2025-01-26 19:53:10 -0500

    Fixed URL ordering error in urls.py

--- easyPoller/main/urls.py
+++ easyPoller/main/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<slug:slug>", views.poll_view, name='poll_view'),
+    path('vote/', views.vote),
     path("<slug:slug>/", views.poll_view, name='poll_view'),
-    path("<slug:slug>/results", views.results_view),
-    path("<slug:slug>/results/", views.results_view),
-    path('vote/', views.vote)
+    path("<slug:slug>/results/", views.results_view)
 ]
